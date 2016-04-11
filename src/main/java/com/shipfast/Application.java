505e1234ca13c8a184f806f8d28dcf8e168f15e9
Abhinav Pobbati <apobbati@codeblock.io>
2016-04-10 21:50:25 -0400

    Fix orders

--- src/main/java/com/shipfast/Application.java
+++ src/main/java/com/shipfast/Application.java
@@ -1,6 +1,7 @@
 package com.shipfast;
 
 import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
@@ -17,6 +19,9 @@ import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 @EnableAutoConfiguration
 @ComponentScan("com.shipfast")
 public class Application extends SpringBootServletInitializer {
+	@Autowired
+	private Environment environment;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -24,7 +29,7 @@ public class Application extends SpringBootServletInitializer {
 	@Configuration
 	static class LocalConfig {
 		public @Bean MongoDbFactory mongoDbFactory() throws Exception {
-			return new SimpleMongoDbFactory(new MongoClient(), "inventory");
+			return new SimpleMongoDbFactory(new MongoClient(), "orders");
 		}
 
 		public @Bean MongoTemplate mongoTemplate() throws Exception {
@@ -36,12 +41,17 @@ public class Application extends SpringBootServletInitializer {
 	@Profile("docker")
 	static class DockerConfig {
 		public @Bean MongoDbFactory mongoDbFactory() throws Exception {
-			return new SimpleMongoDbFactory(new MongoClient("inventory-db"), "inventory");
+			return new SimpleMongoDbFactory(new MongoClient("orders-db"), "orders");
 		}
 
 		public @Bean MongoTemplate mongoTemplate() throws Exception {
 			return new MongoTemplate(mongoDbFactory());
 		}
+	}
+
+	@Bean
+	public Services services() {
+		return new Services(environment);
 	}
 
 	@Bean
