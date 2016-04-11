6bf5b19b68d5f9d545221e058f782d23efc3651b
Abhinav Pobbati <apobbati@codeblock.io>
2016-04-10 22:49:37 -0400

    Add stub for orders model

--- src/main/java/com/shipfast/OrdersService.java
+++ src/main/java/com/shipfast/OrdersService.java
@@ -1,11 +1,17 @@
 package com.shipfast;
 
+import com.shipfast.model.Order;
+import com.shipfast.model.OrderRepository;
 import io.advantageous.qbit.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RequestMapping("orders")
 public class OrdersService {
+	@Autowired
+	private OrderRepository orderRepository;
+
 	@RequestMapping("placeOrder")
-	public String placeOrder() {
+	public String placeOrder(Order order) {
 		return "Hello World";
 	}
 }
---
+++ src/main/java/com/shipfast/model/Order.java
@@ -0,0 +1,36 @@
+package com.shipfast.model;
+
+import org.springframework.data.annotation.Id;
+
+public class Order {
+	@Id
+	private String id;
+
+	private String itemId;
+
+	private Integer quantity;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+}
